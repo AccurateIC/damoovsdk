f789d68bffa8ec3787b967dc0bb46a04d455743e
omkaraccurate <omkar@accurateic.in>
2025-04-23 18:07:35 +0530

    added instrumentation tests

--- app/src/androidTest/java/com/example/accuratedamoov/SplashScreenActivityTest.kt
+++ app/src/androidTest/java/com/example/accuratedamoov/SplashScreenActivityTest.kt
@@ -1,37 +1,68 @@
 package com.example.accuratedamoov
 
-
+import android.content.Intent
 import androidx.test.core.app.ActivityScenario
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import com.example.accuratedamoov.service.NetworkMonitorService
+import com.raxeltelematics.v2.sdk.TrackingApi
+import com.raxeltelematics.v2.sdk.utils.permissions.PermissionsWizardActivity
+import io.mockk.every
+import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-
 
 @RunWith(AndroidJUnit4::class)
-class MockSplashScreenActivityTest {
-
-    // Grant permissions rule
+class SplashScreenActivityTest {
 
     @Test
-    fun testPermissionsGrantedNavigatesToMainActivity() {
-        // Launch SplashScreenActivity
-        ActivityScenario.launch(MockSplashScreenActivity::class.java)
+    fun splashScreen_launches_and_checks_permissions() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(context, SplashScreenActivity::class.java)
 
-        // Wait for splash screen delay + transition
-        Thread.sleep(3000)
+        val scenario = ActivityScenario.launch<SplashScreenActivity>(intent)
 
-        // Check splash is shown
-        onView(withId(R.id.splashscreenll)).check(matches(isDisplayed()))
+        scenario.onActivity { activity ->
 
-        // Check MainActivity view
-        onView(withId(R.id.container)).check(matches(isDisplayed()))
+            assert(activity != null)
+
+        }
     }
 
+    @Test
+    fun whenPermissionsNotGranted_launchesPermissionsWizard() {
+        val scenario = ActivityScenario.launch(SplashScreenActivity::class.java)
+
+        scenario.onActivity { activity ->
+
+            activity.checkPermissionsAndContinue()
+            val expectedIntent = Intent(activity, PermissionsWizardActivity::class.java)
+            assert(expectedIntent.component?.className?.contains("PermissionsWizardActivity") == true)
+        }
+    }
+
+    @get:Rule
+    val rule = IntentsTestRule(SplashScreenActivity::class.java, true, false)
+
+    @Test
+    fun whenPermissionsGrantedAndConnected_navigatesToMainActivity() {
+
+        mockkObject(TrackingApi)
+        every { TrackingApi.getInstance().areAllRequiredPermissionsAndSensorsGranted() } returns true
+        every { TrackingApi.getInstance().isTracking() } returns true
+        NetworkMonitorService.isConnected = true
+
+        rule.launchActivity(null)
+        Thread.sleep(1500)
+        intended(hasComponent(MainActivity::class.java.name))
+
+        unmockkAll()
+    }
 
 
 }
